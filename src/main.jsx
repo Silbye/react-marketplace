f6ad3b41f16a70d6720906480a974299f9c0a576
Silbye <silverxlxl9@gmail.com>
2025-01-06 14:17:21 +0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,7 @@ const router = createBrowserRouter([
     path: "/article/:id",
     element: <Item />,
   },
+  { path: "*", element: <Home /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
